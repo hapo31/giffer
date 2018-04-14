ad0bd0da3828c6af7a1f1fa6b1cc3a70042e487f
happou31 <happo31@gmail.com>
2018-04-15 00:49:19 +0900

    t

---
+++ src/state/AppState.ts
@@ -0,0 +1,7 @@
+export class AppState {
+  constructor(
+    public srcList: string[] = [],
+    public currentTime: number = 0,
+    public time: number = 0
+  ) {}
+}
---
+++ src/state/GifViewer.ts
@@ -0,0 +1,7 @@
+export class GifViewerState {
+  public srcList: string[] = [];
+  public delay: number = 0;
+  public repeat: number = 0;
+  public width: number = 0;
+  public height: number = 0;
+}
