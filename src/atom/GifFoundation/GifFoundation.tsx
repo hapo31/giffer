ebe90330db84f31aa807c0f4efb02d23ef9a8e0a
happo31 <happo31@gmail.com>
2018-04-15 01:35:09 +0900

    文法がおかしかった

--- src/atom/GifFoundation/GifFoundation.tsx
+++ src/atom/GifFoundation/GifFoundation.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 
 type Props = {
-  onMounted: (canvas: HTMLCanvasElement);
+  onMounted: (canvas: HTMLCanvasElement) => void;
   dataUrl: string;
   width: number;
   height: number;
-}
+};
 
 export default class GifFoundation extends React.PureComponent<Props> {
   private canvas: HTMLCanvasElement | null = null;
@@ -15,14 +15,16 @@ export default class GifFoundation extends React.PureComponent<Props> {
   }
 
   render() {
-    return <div className="GIFViewer">
-    <canvas
-      ref={e => (this.canvas = e!)}
-      width={this.props.width}
-      height={this.props.height}
-      style={{ display: "none" }}
-    />
-    <img src={this.props.dataUrl} alt="" />
-  </div>
+    return (
+      <div className="GIFViewer">
+        <canvas
+          ref={e => (this.canvas = e!)}
+          width={this.props.width}
+          height={this.props.height}
+          style={{ display: "none" }}
+        />
+        <img src={this.props.dataUrl} alt="" />
+      </div>
+    );
   }
 }
