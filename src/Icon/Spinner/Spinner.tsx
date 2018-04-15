8d2a861677cb9178a91ca02511dd80ee7647a430
happo31 <happo31@gmail.com>
2018-04-15 18:57:07 +0900

    export defaultに統一

--- src/Icon/Spinner/Spinner.tsx
+++ src/Icon/Spinner/Spinner.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Icon from "./Spinner.svg";
 
-export class Spinner extends React.PureComponent {
+export default class Spinner extends React.PureComponent {
   render() {
     return <Icon />;
   }
