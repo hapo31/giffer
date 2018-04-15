c689fa6be22056e778a476e26d9b1a777022bba2
happo31 <happo31@gmail.com>
2018-04-15 18:56:54 +0900

    パスを調整

--- src/constant/AllowExtensions.ts
+++ src/constant/AllowExtensions.ts
@@ -1,5 +1,5 @@
 export const ALLOW_EXTENSITONS = ["png", "jpg", "jpeg", "bmp"];
 
-export function PassExtensions(fileName: string) {
+export default function PassExtensions(fileName: string) {
   return ALLOW_EXTENSITONS.some(v => fileName.endsWith(v));
 }
